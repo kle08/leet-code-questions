cc43e8d45ac4abc09866e9f05f29234a8515a3ac
Kevin Le <46305303+kle08@users.noreply.github.com>
2019-09-05 23:24:10 -0700

    fix syntax

--- twoSum.js
+++ twoSum.js
@@ -19,8 +19,8 @@ const twoSum = function (nums, target) {
   const comp = {};
   for (let i = 0; i < nums.length; i++) {
     if (comp[nums[i]] >= 0) {
-      return [comp[nums[i]], i]
+      return [comp[nums[i]], i];
     }
-    comp[target - nums[i]] = i
+    comp[target - nums[i]] = i;
   }
 };
