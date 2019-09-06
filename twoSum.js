5154b21141da2f123efe834ead42a4d3d5375d42
iamasianman <kevinle1991@gmail.com>
2019-09-05 23:07:23 -0700

    Completed twoSum with 2 solutions

---
+++ twoSum.js
@@ -0,0 +1,26 @@
+// Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+// rt 96ms mu 37.7mb 
+// time complex 0(n^2)
+const twoSum = function (nums, target) {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        return [i, j];
+      }
+    }
+  }
+}
+
+// best solution rt 44ms
+const twoSum = function (nums, target) {
+  const comp = {};
+  for (let i = 0; i < nums.length; i++) {
+    if (comp[nums[i]] >= 0) {
+      return [comp[nums[i]], i]
+    }
+    comp[target - nums[i]] = i
+  }
+};
