497541a077627752bea3d59ec8661cf8d1a785be
iamasianman <kevinle1991@gmail.com>
2019-12-03 22:58:52 -0800

    implemented LRU Cache method w/ map and DLL

--- LRUCache.js
+++ LRUCache.js
@@ -21,3 +21,50 @@
 // cache.get(1);       // returns -1 (not found)
 // cache.get(3);       // returns 3
 // cache.get(4);       // returns 4
+
+class ListNode {
+  constructor(key, val) {
+    this.key = key;
+    this.val = val;
+    this.prev = this.next = null;
+  }
+}
+
+class LRUCache {
+  constructor(capacity) {
+    this.dummy = this.tail = new ListNode();
+    this.nodes = new Map();
+    this.capacity = capacity;
+  }
+
+  remove(key) {
+    const node = this.nodes.get(key);
+    if (!node) return;
+    node.prev.next = node.next;
+    if (node.next) {
+      node.next.prev = node.prev;
+    } else {
+      this.tail = node.prev;
+    }
+    this.nodes.delete(key);
+  }
+
+  get(key) {
+    if (this.nodes.has(key)) {
+      this.put(key, this.nodes.get(key).val);
+      return this.tail.val;
+    }
+    return -1;
+  }
+
+  put(key, value) {
+    this.remove(key);
+    if (this.nodes.size === this.capacity) {
+      this.remove(this.dummy.next.key);
+    }
+    const oldTail = this.tail;
+    this.tail = oldTail.next = new ListNode(key, value);
+    this.tail.prev = oldTail;
+    this.nodes.set(key, this.tail);
+  }
+}
