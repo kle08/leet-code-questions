401afda846903491b9a658d0b0629e40f9e6f1f4
iamasianman <kevinle1991@gmail.com>
2019-12-04 22:37:51 -0800

    implemented max sub arr problem

--- maxSubArr.js
+++ maxSubArr.js
@@ -8,3 +8,14 @@
 // Follow up:
 
 // If you have figured out the O(n) solution, try coding another solution using the divide and conquer approach, which is more subtle.
+
+function maxSubArray(A) {
+  var prev = 0;
+  var max = -Number.MAX_VALUE;
+
+  for (var i = 0; i < A.length; i++) {
+    prev = Math.max(prev + A[i], A[i]);
+    max = Math.max(max, prev);
+  }
+  return max;
+}
