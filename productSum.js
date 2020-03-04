4d35c44b70cf0ddd82faa8a52082249664094015
iamasianman <kevinle1991@gmail.com>
2020-03-03 19:13:33 -0800

    completed product sum problem

--- productSum.js
+++ productSum.js
@@ -4,5 +4,13 @@
 // results 12 ==> 5 + 2 2* (7-1) +3 + 2 * 6 (3*(-13+8) + 4) 
 
 const productSum = (array, multi = 1) => {
-
+  let sum = 0;
+  for (const element of array) {
+    if (Array.isArray(element)) {
+      sum += productSum(element, multi + 1);
+    } else {
+      sum += element
+    }
+  }
+  return sum * multi;
 }
