4894ec9732ec20796d5cca4184dfaebe5b0d1c08
iamasianman <kevinle1991@gmail.com>
2020-03-03 19:12:23 -0800

    added product sum

---
+++ productSum.js
@@ -0,0 +1,8 @@
+// product sum 
+// function that tkeas in a special array and returns its product sum
+// input [5, 2,[7,-1],3,[6,[-13,8],4]]
+// results 12 ==> 5 + 2 2* (7-1) +3 + 2 * 6 (3*(-13+8) + 4) 
+
+const productSum = (array, multi = 1) => {
+
+}
