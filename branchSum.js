2c4f7bbb3e03b4767b74ff94489937d0db420039
iamasianman <kevinle1991@gmail.com>
2020-03-04 13:57:45 -0800

    added branch sum

---
+++ branchSum.js
@@ -0,0 +1,13 @@
+// you have a tree structure, how would you calculate the sum of eaach branch
+// I - tree structure
+// o - array of the total sum of each branch 
+// e - o(n), s- o(n)
+// c - if there is only 1 node return 
+
+class BinaryTree {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
