597d747f4ed49c998d5b6e9132da4a4c341c5f15
iamasianman <kevinle1991@gmail.com>
2020-03-04 14:08:15 -0800

    branchSum completed

--- branchSum.js
+++ branchSum.js
@@ -11,3 +11,23 @@ class BinaryTree {
     this.right = null;
   }
 }
+
+const branchSum = (root) => {
+  const sums = [];
+  calBranchSum(root, 0, sums);
+  return sums
+}
+
+const calBranchSum = (node, runningSum, sums) => {
+  if (!node) return;
+
+  let newRuningSum = node.value + runningSum;
+
+  if (!node.left && !node.right) {
+    sums.push(newRuningSum);
+    return;
+  }
+  calBranchSum(node.left, newRuningSum, sums);
+  calBranchSum(node.right, newRuningSum, sums);
+
+}
