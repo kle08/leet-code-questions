9017ef75cb45f414afdb1d05cf99561915ff718b
iamasianman <kevinle1991@gmail.com>
2020-03-09 10:01:49 -0700

    added bubble sort and implented it

---
+++ bubbleSort.js
@@ -0,0 +1,40 @@
+//space - O(n)
+// time - worst O(n^2) best - O(n) if it is sorted
+
+function bubbleSort(arr) {
+  var noSwaps;
+  for (var i = arr.length; i > 0; i--) {
+    noSwaps = true;
+    for (var j = 0; j < i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        var temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+        noSwaps = false;
+      }
+    }
+    if (noSwaps) break;
+  }
+  return arr;
+}
+
+function bubbleSort(array) {
+  let isSorted = false;
+  let counter = 0;
+  while (!isSorted) {
+    isSorted = true;
+    for (let i = 0; i < array.length - 1 - counter; i++) {
+      if (array[i] > array[i + 1]) {
+        swap(i, i + 1, array);
+        isSorted = false;
+      }
+    }
+    counter++;
+  }
+  return array;
+}
+function swap(i, j, array) {
+  let temp = array[j];
+  array[j] = array[i];
+  array[i] = temp;
+}
