c33ed8b10119af8dd038721dcf1aa6d0fe1f3e29
iamasianman <kevinle1991@gmail.com>
2020-03-11 16:09:54 -0700

    implemented closest value in BST

--- closestValueBST.js
+++ closestValueBST.js
@@ -3,4 +3,35 @@
 // output  value closest to target 
 
 function findClosestValueBST(tree, target) {
+  return helper(tree, target, Infinity);
+}
+function helper(tree, target, closest) {
+  if (tree === null) return closest;
+  if (Math.abs(target - closest) > Math.abs(target - tree.node)) {
+    closest = tree.value;
+  }
+  if (target < closest) {
+    return helper(tree.left, target, closest)
+  }
+  if (target > closest) {
+    return helper(tree.right, target, closest)
+  } else {
+    return closest
+  }
+}
+
+function helper(tree, target, closest) {
+  let currentNode = tree;
+  while (currentNode !== null) {
+    if (Math.abs(target - closest) > Math.abs(target - currentNode.value)) {
+      closest = currentNode.value;
+    }
+    if (target < currentNode.vaule) {
+      currentNode = currentNode.left
+    } else if (target > currentNode.vaule) {
+      currentNode = currentNode.right
+    } else {
+      break;
+    }
+  }
 }
