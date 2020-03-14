0bb94e282a854c6102c6cf8a3160512ee0d3b968
iamasianman <kevinle1991@gmail.com>
2020-03-14 14:44:19 -0700

    added insort sort

---
+++ insertSort.js
@@ -0,0 +1,8 @@
+// insertion sort 
+// space - O(n)
+// Time - O(n^2)
+// basically insertion sort is whre you start on index 1 and compare to the next index and swap 
+// left side is slowing sorting to the right side 
+
+function inserSort(array) {
+}
