df91929472522985893f24eda67cf30acb977ee9
iamasianman <kevinle1991@gmail.com>
2020-03-15 23:16:55 -0700

    added seleciton sort

---
+++ selectionSort.js
@@ -0,0 +1,11 @@
+// selection sort
+// space - O(n)
+// time - O(n^2)
+// basically iterate each index to find the smallest value once found 
+// swap the first index and the smallest index
+// then iterate again to find the next smallest and swap 
+// repeat
+
+function selecitonSort(array) {
+
+}
