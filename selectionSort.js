2b40872724f1d25e08250d3ca77459b26eb70cd6
iamasianman <kevinle1991@gmail.com>
2020-03-15 23:27:32 -0700

    implemented selection sort

--- selectionSort.js
+++ selectionSort.js
@@ -7,5 +7,22 @@
 // repeat
 
 function selecitonSort(array) {
+  let startIdx = 0;
+  while (startIdx < array.length - 1) {
+    let smallestIdx = startIdx;
+    for (let i = startIdx + 1; i < array.length; i++) {
+      if (array[startIdx] < array[i]) {
+        smallestIdx = i;
+      }
+      swap(startIdx, smallestIdx, array);
+      startIdx++;
+    }
+    return array;
+  }
+}
 
+function swap(i, j, array) {
+  let temp = array[j];
+  array[j] = array[i];
+  array[i] = temp;
 }
