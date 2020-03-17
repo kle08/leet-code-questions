ee0a5d95f2808d3adc31091764e0a0e698162cb4
iamasianman <kevinle1991@gmail.com>
2020-03-16 21:54:05 -0700

    added fib problem

---
+++ fibNth.js
@@ -0,0 +1,8 @@
+// fib nth #
+// the pattern is basically the first 2 numbers is 0 and 1
+// the next # is the 2 previous number added together 
+// 0, 1, 1, 2, 3, 5, 8, etc
+
+function fib(n) {
+
+}
