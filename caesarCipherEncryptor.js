eae985c3016ceafb06d0e7edceaa584a25bee9db
iamasianman <kevinle1991@gmail.com>
2020-03-17 22:52:31 -0700

    added caesar cipher encryptor problem

---
+++ caesarCipherEncryptor.js
@@ -0,0 +1,9 @@
+// have 2 inputs sting and key
+// key is used to determine how many letters is being shifted to the next letter.
+// example string - xyz key - 2 
+// output = zab since the key is 2 it will go up 2 letters of the alpha
+// after z it will restart back to 'A'
+
+function caesarCE(string, key) {
+
+}
