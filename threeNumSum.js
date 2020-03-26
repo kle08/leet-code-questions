48fb9d9fc3635290084790c34471c113b25fdd11
iamasianman <kevinle1991@gmail.com>
2020-03-25 23:25:24 -0700

    added three sum problem

---
+++ threeNumSum.js
@@ -0,0 +1,10 @@
+// 3 number sum
+// input - you have an array and target value 
+// output - array of 3 values eqaul to target 
+// iterate the array to find the 3 values that would equal to the some 
+// [1,2,3,4,5,6] 6 
+// [1,2,3]
+
+function threeNumSum(array, target) {
+
+}
