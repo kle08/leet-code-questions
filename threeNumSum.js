448e142a05257255826d52157a22918ce4678eaf
iamasianman <kevinle1991@gmail.com>
2020-03-25 23:45:34 -0700

    implemented three number sum problem

--- threeNumSum.js
+++ threeNumSum.js
@@ -6,5 +6,25 @@
 // [1,2,3]
 
 function threeNumSum(array, target) {
-
+  array.sort((a, b) => a - b);
+  const triplets = [];
+  for (let i = 0; i < array.length - 2; i++) {
+    let left = i + 1;
+    let right = array.length - 1;
+    while (left < right) {
+      const currentSum = array[i] + array[left] + array[right];
+      if (currentSum === target) {
+        triplets.push(array[i], array[left], array[right]);
+        left++;
+        right++;
+      } else if (currentSum < target) {
+        left++;
+      } else if (currentSum > target) {
+        right++;
+      }
+    }
+  }
+  return triplets;
 }
+
+console.log(threeNumSum([1, 2, 3, 4, 5, 6, 7, 8, 9, 15], 18));
