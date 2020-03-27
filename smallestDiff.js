8827e41d43bb08906b4a577aafbab0aa6760e1e2
iamasianman <kevinle1991@gmail.com>
2020-03-26 23:34:34 -0700

    added smallest diff problem

---
+++ smallestDiff.js
@@ -0,0 +1,12 @@
+// smallest diff
+// input - 2 arrays of integers
+// output - an array of 2 values from each array that has the smallest diff between them
+// the purpose of this algo is to determine what 2 values, 1 from each array to find the diff between them
+// example [2,3,15,20], [40,30,25, 21]
+// should return [20,21];
+// best way to tackle this type of problem is to use the 2 pointer method
+// compare each indexies to determine to move the poiter up on either array
+
+function smallestDiff(arr1, arr2) {
+
+}
