70123f98f3822f34d36670959fa522523ca342c9
iamasianman <kevinle1991@gmail.com>
2020-03-31 23:38:02 -0700

    added move element to end problem

---
+++ moveEleToEnd.js
@@ -0,0 +1,9 @@
+// move target elements to the end 
+// algo basically just asking to move all the target elements to the end of the array 
+// input is an array of int and target value 
+// example [1,2,1,3,4,1] , 1 (target )
+// output - [2,3,4,1,1,1]
+
+function moveToEnd(arr, target) {
+
+}
