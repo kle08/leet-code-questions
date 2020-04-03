1d1b0d6e5d4c40bd3431ba7a025ddaf89c53bcff
iamasianman <kevinle1991@gmail.com>
2020-04-02 23:25:46 -0700

    added kadane algo

---
+++ kadaneAlgo.js
@@ -0,0 +1,12 @@
+// Kadane Algo
+// purpose of kadane algo is to find the max consecutive sum in a sub array in the array
+// input - array of numbers 
+// output - the sum of the highest value of consecutive numbers in hte array/sub array
+// t - O(n)
+// s - O(1)
+// [ 3,5,-9,1,3-2,7,2]
+// = 9 (cuz it sumed up 7,2 as the highest value)
+
+function kadane(array) {
+
+}
