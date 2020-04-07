b2982fdb99af8e9380c5693d8d46ac61ee0be56b
iamasianman <kevinle1991@gmail.com>
2020-04-06 22:13:17 -0700

    added validate bst problem

---
+++ validateBST.js
@@ -0,0 +1,18 @@
+//validate BST
+// create a function that it would validate a binary search tree
+// input - a tree
+// output - boolean
+// parent has a middle value where the left child/node is less than parent
+// where as right node is greater than parent
+
+class BST {
+  constructor(value) {
+    this.value = value;
+    this.right = null;
+    this.left = null;
+  }
+}
+
+function valBST(tree) {
+
+}
