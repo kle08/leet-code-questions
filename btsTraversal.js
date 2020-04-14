31eb7da07feab2b135cb295e60041066a0f9c18c
iamasianman <kevinle1991@gmail.com>
2020-04-13 23:39:35 -0700

    added bst traversal

---
+++ btsTraversal.js
@@ -0,0 +1,25 @@
+// BST traversal 
+// wrte 3 fx using in-order, preorder and post order tree traversal
+// in-order 
+// input - tree
+// output - an array of the tree that is from least to greatest 
+//pre-order (dfs)
+// input - tree
+// output - array that the root is the first index then start left side of the tree
+// until null left side then right side of the left child.
+// then right side of the tree
+// post-order (bfs)
+// input - tree
+// output - array where the first indes of the array is the furthest left leaf goes up then the irght side and last idx is the root
+
+function inOrderTrav(tree, array) {
+
+}
+
+function preOrderTrav(tree, array) {
+
+}
+
+function postOrderTrav(tree, array) {
+
+}
