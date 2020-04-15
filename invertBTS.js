c27e13129322ce54ab66a3544969da61998059f7
iamasianman <kevinle1991@gmail.com>
2020-04-14 23:44:54 -0700

    added invert Binary tree problem

---
+++ invertBTS.js
@@ -0,0 +1,18 @@
+// invert binary tree
+// createa function to flip a binary tree
+// input is a tree 
+// ex:      1       
+//       /   \
+//       2    3 
+//      /\    /\
+//     4  5   6 7
+// output - 
+// ex:      1       
+//       /   \
+//       3    2 
+//      /\    /\
+//     6  7   4 5
+
+function invertBT(tree) {
+
+}
