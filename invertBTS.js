b9af735cc83cf13eaf2ebe24575e8394553e558a
iamasianman <kevinle1991@gmail.com>
2020-04-14 23:53:43 -0700

    solve invert bt recursivly and iteratively

--- invertBTS.js
+++ invertBTS.js
@@ -13,6 +13,33 @@
 //      /\    /\
 //     6  7   4 5
 
-function invertBT(tree) {
+// // recursion way to solve it T - O(n), S - O(d)
+// function invertBT(tree) {
+//   if (tree === null) return;
+//   swapLeftRight(tree);
+//   invertBT(tree.left);
+//   invertBT(tree.right);
+// }
 
+// function swapLeftRight(tree) {
+//   const left = tree.right;
+//   tree.left = tree.right;
+//   tree.right = left;
+// }
+
+/// iterate version 
+function invertBT(tree) {
+  const que = [tree];
+  while (que.length) {
+    const current = que.shift();
+    if (current === null) continue;
+    swapLeftRight(current);
+    que.push(current.left);
+    que.push(current.right);
+  }
+}
+function swapLeftRight(tree) {
+  const left = tree.right;
+  tree.left = tree.right;
+  tree.right = left;
 }
