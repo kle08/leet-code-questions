b123359bb614ad3d1ca914292d5d6ed32d7bb797
iamasianman <kevinle1991@gmail.com>
2020-04-16 23:52:23 -0700

    added search value in matrix problem

---
+++ searchSoredMatrix.js
@@ -0,0 +1,9 @@
+// search in sorted matrix 
+// create a fucntion to find the target value in a sorted matrix 
+// input - matrix and target
+// output - location of the target 
+// edge - if the target is not in the martix return [-1, -1]
+
+function searchMatrix(matrix, target) {
+
+}
