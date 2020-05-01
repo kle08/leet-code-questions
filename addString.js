d966c02f44a2c0182c388429c3a08a3787132ebf
iamasianman <kevinle1991@gmail.com>
2020-04-30 21:41:14 -0700

    added add string problem

---
+++ addString.js
@@ -0,0 +1,10 @@
+// Add strings 
+// given 2 non negative nubmers, represent them as a string return the sum of both var
+// Constraints - cant use built in biginteger libary or conver the inputs to integer 
+// length of both var is < 500
+// both var contains only 0-9 digits 
+// does not conatin any leading zero's
+
+function addStrings(num1, num2) {
+
+}
