57b50567bbe25b46bac8d239c646c8595f2e8d95
iamasianman <kevinle1991@gmail.com>
2020-04-30 23:39:26 -0700

    implemented add string

--- addString.js
+++ addString.js
@@ -6,5 +6,24 @@
 // does not conatin any leading zero's
 
 function addStrings(num1, num2) {
-
+  let a = num1.split("").reverse().join("");
+  let b = num2.split("").reverse().join("");
+  let len = Math.max(a.length, b.length)
+  // let len = a.length > b.length ? a.length : b.length;
+  result = [];
+  for (let i = 0; i < len; i++) {
+    num1 = Number(a[i] || 0);
+    num2 = Number(b[i]) || 0;
+    curr = Number(result[i] || 0) + num1 + num2
+    if (curr >= 10) {
+      result[i] = curr % 10;
+      result.push(1);
+    }
+    else {
+      result[i] = curr;
+    }
+  }
+  return result.reverse().join("");
 }
+
+console.log(addStrings('16523', '6823'));
