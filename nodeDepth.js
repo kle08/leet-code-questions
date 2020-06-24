8174a38911d693f90cf0a9b7aa06eaca69dc6f31
iamasianman <kevinle1991@gmail.com>
2020-06-23 22:08:05 -0700

    added node depth problem

---
+++ nodeDepth.js
@@ -0,0 +1,15 @@
+// Given a tree find the total value of the longest branch.
+
+
+function nodeDepths(root) {
+
+}
+
+// This is the class of the input binary tree.
+class BinaryTree {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
